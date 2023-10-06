c6948db1778983d32da2aed6c9000c5d0d693762
rae <raej0000@gmail.com>
2023-10-06 09:48:51 -0700

    replace icon.png with download.png and rename

--- src/main/java/io/github/Tors_0/dotwarden/common/DOTWarden.java
+++ src/main/java/io/github/Tors_0/dotwarden/common/DOTWarden.java
@@ -18,22 +18,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DOTWarden implements ModInitializer {
-	public static final String ID = "dotwarden";
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+    public static final String ID = "dotwarden";
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod name as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(ID);
     public static final SpecialRecipeSerializer<HarmonicStaffRecipe> HARMONIC_STAFF_RECIPE = RecipeSerializer.register(
             "dotwarden:harmonic_staff", new SpecialRecipeSerializer<>(HarmonicStaffRecipe::new));
     public static final SpecialRecipeSerializer<SeismicHornRecipe> SEISMIC_HORN_RECIPE = RecipeSerializer.register(
             "dotwarden:seismic_horn", new SpecialRecipeSerializer<>(SeismicHornRecipe::new));
     private static final Identifier WARDEN_LOOT_TABLE_ID = EntityType.WARDEN.getLootTableId();
 
-	@Override
-	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Now initializing {} version {}", mod.metadata().name(), mod.metadata().version());
+    @Override
+    public void onInitialize(ModContainer mod) {
+        LOGGER.info("Now initializing {} version {}", mod.metadata().name(), mod.metadata().version());
 
-		ModItems.init();
+        ModItems.init();
 
         ServerPlayerEntityCopyCallback.EVENT.register((copy, original, wasDeath) -> {
             if (copy instanceof PlayerExtensions playerNew && original instanceof PlayerExtensions playerOld) {
@@ -48,5 +48,5 @@ public class DOTWarden implements ModInitializer {
                 tableBuilder.pool(poolBuilder);
             }
         });
-	}
+    }
 }
